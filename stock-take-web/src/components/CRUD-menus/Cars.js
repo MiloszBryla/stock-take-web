047333a5ee648509c1744401c0cad2b5eb587426
MiloszBryla <bryla.milosz@gmail.com>
2021-02-17 20:33:18 +0100

    advance car view

--- stock-take-web/src/components/CRUD-menus/Cars.js
+++ stock-take-web/src/components/CRUD-menus/Cars.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import {NavLink} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
 import "../../styling/index.css"
+import {NavLink} from "react-router-dom";
 
 function Cars() {
+
+    const [fetchedCars, setCars] = useState([]);
+
+    const fetchCars = async () => {
+
+        const response = await fetch(`http://localhost:8080/api/cars`,
+            {
+                method: 'GET',
+            });
+        const cars = await response.json();
+        console.log(cars);
+        setCars(cars);
+    }
+
+    useEffect(() => {fetchCars()}, []);
+
     return (
         <div>
-            <div className={"title"}>Cars</div>
-
+            <span><NavLink to={"/"}>&lt; back</NavLink></span>
+            <h4 className={"title"}>CARS</h4>
+            <table className="cars-list">
+                <tr>
+                    <th>ID</th>
+                    <th>MANUFACTURER</th>
+                    <th>REGISTRATION</th>
+                    <th>IS RENT</th>
+                    <th>SERVICE</th>
+                    <th>FUEL</th>
+                </tr>
+                {fetchedCars.map(theCar => (
+                    <tr>
+                        <td>{theCar.id}</td>
+                        <td>{theCar.brandName}</td>
+                        <td>{theCar.carId}</td>
+                        <td>{theCar.rent}</td>
+                        <td>{theCar.service}</td>
+                        <td>{theCar.fuel}</td>
+                    </tr>
+                    ))}
+            </table>
         </div>
     );
 }
