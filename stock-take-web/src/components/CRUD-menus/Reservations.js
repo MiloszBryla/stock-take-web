99ac66bebdd5b7a9ce9096c1f459f12f1ab142f6
michal <kuk.michal3@gmail.com>
2021-02-18 12:36:29 +0100

    add reservations

--- stock-take-web/src/components/CRUD-menus/Reservations.js
+++ stock-take-web/src/components/CRUD-menus/Reservations.js
@@ -1,11 +1,50 @@
-import React from 'react';
+
+import React, {useEffect, useState} from 'react';
 import {NavLink} from "react-router-dom";
 import "../../styling/index.css"
 
 function Reservations() {
+    const [fetchedReservations, setReservation] = useState([]);
+
+    const fetchReservations = async () => {
+
+        const response = await fetch(`http://localhost:8080/api/reservations`,
+            {
+                method: 'GET',
+            });
+        const reservations = await response.json();
+        console.log(reservations);
+        setReservation(reservations);
+    }
+
+    useEffect(() => {fetchReservations()}, []);
+
+
     return (
         <div>
-            <div className={"title"}>Reservations</div>
+            <span><NavLink to={"/"}>&lt; back</NavLink></span>
+            <h4 className={"title"}>RESERVATIONS</h4>
+            <table className="cars-list">
+                <tr>
+                    <th>TRANSACTION ID</th>
+                    <th>RENTER ID</th>
+                    <th>CAR ID</th>
+                    <th>TOTAL PRICE (PLN)</th>
+                    <th>START-DATE</th>
+                    <th>END-DATE</th>
+                </tr>
+                {fetchedReservations.map(reservation => (
+                    <tr>
+                        <td>{reservation.id}</td>
+                        <td>{reservation.renterId}</td>
+                        <td>{reservation.carId}</td>
+                        <td>{reservation.price}</td>
+                        <td>{reservation.startDate}</td>
+                        <td>{reservation.endDate}</td>
+
+                    </tr>
+                ))}
+            </table>
         </div>
     );
 }
